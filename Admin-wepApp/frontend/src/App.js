f0be9474253a9c31780a09972f4d7bb84587ff89
Korantengnator2 <nana.koranteng@ashesi.edu.gh>
2026-02-01 15:24:36 +0000

    test: update homepage text

--- Admin-wepApp/frontend/src/App.js
+++ Admin-wepApp/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+		EcoDrone - Drone Management System (DEV)
         </p>
         <a
           className="App-link"
