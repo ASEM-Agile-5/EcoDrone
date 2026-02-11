f3c92438697f55b5c66ee8345464103ed88cb9a1
Korantengnator <95726333+Korantengnator@users.noreply.github.com>
2026-02-11 11:55:01 +0000

    feat: add Firebase frontend URLs to CORS settings

--- ecodrone_django/ecodrone_django/settings.py
+++ ecodrone_django/ecodrone_django/settings.py
@@ -37,6 +37,26 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
