8d00a00f40cbb6c141f3deeb851eeefbbb55b43b
Korantengnator <95726333+Korantengnator@users.noreply.github.com>
2026-02-15 20:16:48 +0000

    feat: add Cloud SQL PostgreSQL database with custom user models

--- ecodrone_django/ecodrone_django/settings.py
+++ ecodrone_django/ecodrone_django/settings.py
@@ -91,16 +91,30 @@ WSGI_APPLICATION = 'ecodrone_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST',"localhost"),
-        'PORT': os.getenv('DB_PORT',"8054"),
+if os.getenv('GAE_APPLICATION'):
+    # Running on Cloud Run - use Cloud SQL via Unix socket
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': '/cloudsql/' + os.environ.get('CLOUD_SQL_CONNECTION_NAME', ''),
+            'NAME': os.environ.get('DB_NAME', 'ecodrone_dev'),
+            'USER': 'postgres',
+            'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+            'PORT': '',
         }
-}
+    }
+else:
+    # Running locally - use your local PostgreSQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME', 'ecodrone_local'),
+            'USER': os.getenv('DB_USER', 'postgres'),
+            'PASSWORD': os.getenv('DB_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', '8054'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -137,3 +151,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Custom User Model
+AUTH_USER_MODEL = 'users.User'
