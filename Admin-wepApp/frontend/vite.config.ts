9f6c3784c8e2d5247b835c7c8d06dbe6412ebca4
Patrick Adu-Amankwah <p.a.amankwah@gmail.com>
2026-03-25 14:53:14 +0000

    update

--- Admin-wepApp/frontend/vite.config.ts
+++ Admin-wepApp/frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api/': {
-        target: 'http://localhost:8000/',
+        target: 'https://ecodrone-backend-dev-lwunguolhq-uc.a.run.app/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
