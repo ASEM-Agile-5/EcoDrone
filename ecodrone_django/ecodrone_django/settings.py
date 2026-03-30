916ea973922991dbfa5c3f2b75f525c8b402865b
Patrick Adu-Amankwah <p.a.amankwah@gmail.com>
2026-03-30 19:02:10 +0000

    try 3

--- ecodrone_django/ecodrone_django/settings.py
+++ ecodrone_django/ecodrone_django/settings.py
@@ -41,8 +41,34 @@ SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-98t#7q!-0vm=1&j-bw=zounhdlg
 #     "http://localhost:8000",
 #     "http://127.0.0.1:8000",
 # ]
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = [
+    'https://ecodrone-backend-dev-lwunguolhq-uc.a.run.app',
+    'https://*.run.app',  # Trust all Cloud Run domains
+]
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
